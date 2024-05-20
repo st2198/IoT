8dd916b53f2dddcb316c750b08f40e525af55594
Sviatoslav O <s.terletskyi@ocado.com>
2024-05-20 22:37:48 +0300

    upd

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,10 +1,8 @@
-console.log(process.env.POSTGRESDB_HOST)
 module.exports = {
-  HOST: process.env.POSTGRESDB_HOST,
-  USER: process.env.POSTGRESDB_USER,
-  PASSWORD: process.env.POSTGRESDB_ROOT_PASSWORD,
-  DB: process.env.POSTGRESDB_DATABASE,
-  port: process.env.POSTGRESDB_LOCAL_PORT,
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "mysecretpassword",
+  DB: "measurements",
   dialect: "postgres",
   pool: {
     max: 5,
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: `http://localhost:${process.env.NODE_LOCAL_PORT}`
 };
 
 app.use(cors(corsOptions));
